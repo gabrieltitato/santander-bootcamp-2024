d9dd140819e017b82dc879720089dbf2c096b3e0
gennun <titatoou2@hotmail.com>
2024-07-26 15:48:55 -0300

    Primeira tentativa de publicação do Railway

--- src/main/java/me/dio/domain/repository/UserRepository.java
+++ src/main/java/me/dio/domain/repository/UserRepository.java
@@ -3,5 +3,7 @@ package me.dio.domain.repository;
 import me.dio.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Repository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String account);
 }
